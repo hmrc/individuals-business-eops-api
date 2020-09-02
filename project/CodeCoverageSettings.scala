7a6bdee87b7fe057bf8d8cd465bee24eb9d7bf71
BenCosfordHMRC <21200285+BenCosfordHMRC@users.noreply.github.com>
2020-09-02 12:00:53 +0100

    added dodo around coverage

--- project/CodeCoverageSettings.scala
+++ project/CodeCoverageSettings.scala
@@ -33,6 +33,7 @@ object CodeCoverageSettings {
 
   val settings: Seq[Setting[_]] = Seq(
     ScoverageKeys.coverageExcludedPackages := excludedPackages.mkString(";"),
+    //TODO REVERT BACK TO 95 WHEN ENDPOINT IS AVAILABLE
     ScoverageKeys.coverageMinimum := 75,
     ScoverageKeys.coverageFailOnMinimum := true,
     ScoverageKeys.coverageHighlighting := true
