20321f0f1e7ea6ead956cd6f47fb3c0df62bde59
Harry Ritchie <111759395+harry-ritchie@users.noreply.github.com>
2022-11-03 14:51:55 +0000

    MTDSA-13417: Updating IntegrationBaseSpec

--- it/support/IntegrationBaseSpec.scala
+++ it/support/IntegrationBaseSpec.scala
@@ -32,17 +32,19 @@ trait IntegrationBaseSpec extends UnitSpec with WireMockHelper with GuiceOneServ
   lazy val client: WSClient = app.injector.instanceOf[WSClient]
 
   def servicesConfig: Map[String, Any] = Map(
-    "microservice.services.des.host" -> mockHost,
-    "microservice.services.des.port" -> mockPort,
-    "microservice.services.ifs.host" -> mockHost,
-    "microservice.services.ifs.port" -> mockPort,
-    "microservice.services.mtd-id-lookup.host" -> mockHost,
-    "microservice.services.mtd-id-lookup.port" -> mockPort,
-    "microservice.services.auth.host" -> mockHost,
-    "microservice.services.auth.port" -> mockPort,
+    "microservice.services.des.host"               -> mockHost,
+    "microservice.services.des.port"               -> mockPort,
+    "microservice.services.ifs.host"               -> mockHost,
+    "microservice.services.ifs.port"               -> mockPort,
+    "microservice.services.tys-ifs.host"           -> mockHost,
+    "microservice.services.tys-ifs.port"           -> mockPort,
+    "microservice.services.mtd-id-lookup.host"     -> mockHost,
+    "microservice.services.mtd-id-lookup.port"     -> mockPort,
+    "microservice.services.auth.host"              -> mockHost,
+    "microservice.services.auth.port"              -> mockPort,
     "microservice.services.mtd-api-nrs-proxy.host" -> mockHost,
     "microservice.services.mtd-api-nrs-proxy.port" -> mockPort,
-    "auditing.consumer.baseUri.port" -> mockPort
+    "auditing.consumer.baseUri.port"               -> mockPort
   )
 
   override implicit lazy val app: Application = new GuiceApplicationBuilder()
